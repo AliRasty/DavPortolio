458dd0f4c7c6c259db6a7fc4095719883f95f0e9
ali Rasty <alirasty1997@gmail.com>
2022-12-16 17:08:11 +0330

    init

---
+++ DevPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
