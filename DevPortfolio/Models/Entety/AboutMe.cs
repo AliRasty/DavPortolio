1fb6152215611caef0d7beee32a64a55a8db0979
ali Rasty <alirasty1997@gmail.com>
2022-12-16 23:27:35 +0330

    Add Tamplate And Add ViewComponent

---
+++ DevPortfolio/Models/Entety/AboutMe.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.AccessControl;
+
+namespace DevPortfolio.Models.Entety
+{
+    public class AboutMe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+       
+        
+
+        public AboutMe(int id, string title, string name)
+        {
+            Id = id;
+            Title = title;
+            Name = name;
+        }
+    }
+}
---
+++ DevPortfolio/ViewComponents/SiteViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevPortfolio.Models.Entety;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevPortfolio.ViewComponents
+{
+    public class AboutSiteViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public  IViewComponentResult Invoke()
+        {
+            var about = new List<AboutMe>
+            {
+                new AboutMe(1,"تاریخ تولد","1377"),
+                new AboutMe(2,"وبسایت","-"),
+                new AboutMe(3,"تلفن","09168467533"),
+                new AboutMe(4,"شهر","خرم اباد"),
+                new AboutMe(8,"سن","25"),
+                new AboutMe(6,"درجه","Sinior"),
+                new AboutMe(7,"ایمیل","Alirasty1997@gmail.com")
+             
+            };
+            return View("AboutSite",about);
+        }
+    }
+}
